d5e5c8436dbb77a7ab15f1af8dcfa03d4f6e4d84
Palmer Turley <palmerturley34@gmail.com>
2023-12-18 11:02:58 -0700

    remove nested for loop

--- 2023/day_2/solution.py
+++ 2023/day_2/solution.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 import operator
 from functools import reduce
 
-def main(part=1):
+def main():
     max_values = {
         'red': 12,
         'green': 13,
@@ -14,24 +14,20 @@ def main(part=1):
         game, all_cubes = line.strip().split(': ')
         game_id = int(game.strip('Game '))
         rounds = all_cubes.split('; ')
+        cube_counts = reduce(operator.add, [round.split(', ') for round in rounds])
         valid_game = True
         cube_minimums = defaultdict(int)
-        for round in rounds:
-            cube_counts = round.split(', ')
-            for cube in cube_counts:
-                count, color = cube.split()
-                cube_minimums[color] = max(cube_minimums[color], int(count))
-                if int(count) > max_values[color]:
-                    valid_game = False
+        for cube in cube_counts:
+            count, color = cube.split()
+            cube_minimums[color] = max(cube_minimums[color], int(count))
+            if int(count) > max_values[color]:
+                valid_game = False
         game_power = reduce(operator.mul, cube_minimums.values())
         power_sums += game_power
         if valid_game:
             valid_game_sum += game_id
-    if part == 1:
-        print(f'Part 1: {valid_game_sum}')
-    else:
-        print(f'Part 2: {power_sums}')
+    print(f'Part 1: {valid_game_sum}')
+    print(f'Part 2: {power_sums}')
 
 if __name__ == '__main__': 
     main()
-    main(2)
